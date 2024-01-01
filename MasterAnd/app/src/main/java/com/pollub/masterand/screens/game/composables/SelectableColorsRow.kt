a45e109ec88556ebc9f8253b19ed0f6185796b2c
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2024-01-02 00:59:59 +0100

    feat: add button color change animation

--- MasterAnd/app/src/main/java/com/pollub/masterand/screens/game/composables/SelectableColorsRow.kt
+++ MasterAnd/app/src/main/java/com/pollub/masterand/screens/game/composables/SelectableColorsRow.kt
@@ -1,5 +1,9 @@
 package com.pollub.masterand.screens.game.composables
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
@@ -9,12 +13,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SelectableColorsRow(colors: List<Color>, clickAction: ((Int) -> Unit)?) {
+fun SelectableColorsRow(colors: List<Color>, clickAction: (Int) -> Unit = {}) {
+    val animatedColors = colors.map {
+        animateColorAsState(
+            targetValue = it,
+            animationSpec = repeatable(
+                iterations = 5,
+                animation = tween(durationMillis = 500),
+                repeatMode = RepeatMode.Reverse
+            ),
+            label = "Color_{$it}"
+        )
+    }
+
     Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-        colors.forEachIndexed { index, color ->
+        animatedColors.forEachIndexed { index, animatedColor ->
             CircularButton(
-                onClick = { clickAction?.invoke(index) },
-                color = color,
+                onClick = { clickAction.invoke(index) },
+                color = animatedColor.value,
                 modifier = Modifier.size(50.dp)
             )
         }
