6868eadd8bebfe575a3976ed4d91d75f059b9cb5
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2024-01-01 23:39:11 +0100

    feat: add feedback circles animation

--- MasterAnd/app/src/main/java/com/pollub/masterand/screens/game/composables/FeedbackCircles.kt
+++ MasterAnd/app/src/main/java/com/pollub/masterand/screens/game/composables/FeedbackCircles.kt
@@ -1,10 +1,14 @@
 package com.pollub.masterand.screens.game.composables
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -12,11 +16,19 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun FeedbackCircles(colors: List<Color>) {
     val spacedBy = 2.dp
+
+    val colorAnimations = List(4) { remember { Animatable(Color.White) } }
+    LaunchedEffect(colors) {
+        colorAnimations.forEachIndexed { index, animation ->
+            animation.animateTo(colors[index], animationSpec = tween(250))
+        }
+    }
+
     Column(verticalArrangement = Arrangement.spacedBy(spacedBy)) {
-        colors.chunked(2).forEach { rowColors ->
+        colorAnimations.chunked(2).forEach { rowColorAnimations ->
             Row(horizontalArrangement = Arrangement.spacedBy(spacedBy)) {
-                rowColors.forEach { color ->
-                    SmallCircle(color = color, modifier = Modifier.size(24.dp))
+                rowColorAnimations.forEach { colorAnimation ->
+                    SmallCircle(color = colorAnimation.value, modifier = Modifier.size(24.dp))
                 }
             }
         }
